637681312233ba4e3d7b3cdf5965acab724569d2
Arghyadip Mondal <arghya_2004@Agmon.Agmon>
2024-11-11 10:19:29 +0530

    seventh commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 // src/components/Navbar.jsx
 function Navbar() {
   return (
-    <nav className="flex items-center justify-start px-4 py-4 bg-black text-white"> {/* Changed justify-between to justify-start and reduced padding */}
-      <img src="/istockphoto-1128250636-612x612.jpg" alt="Logo" className="mr-2 w-13 h-14 " /> {/* Added width and height classes */}
+    <nav className="flex items-center justify-start px-4 py-4 bg-black text-white">
+      <img src="/istockphoto-1128250636-612x612.jpg" alt="Logo" className="mr-2 w-15 h-14 rounded-full" /> {/* Changed to rounded-full */}
       <div className="text-2xl font-bold">GelTechInnovators</div>
-      <ul className="flex space-x-8 ml-auto"> {/* Added ml-auto to push the buttons to the right */}
+      <ul className="flex space-x-8 ml-auto">
         <button className="px-4 py-2 rounded-lg transition duration-500 hover:bg-gray-600">
           Home
         </button>
